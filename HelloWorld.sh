98da71c4d97de9732c9b89dc9a3178916fe251dd
Lillyfrand <TheSilentFyrd@gmail.com>
2020-09-03 11:51:26 -0600

    Hello World!

---
+++ HelloWorld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#example of a comment!@#!@$%
+echo "Hello	World"
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
